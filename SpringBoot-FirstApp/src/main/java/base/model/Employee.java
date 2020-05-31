b4621c61eadfd4031092f9579d3b8de698d811a6
SAM <sam062>
2020-05-31 19:50:50 +0530

    Added SpringBoot First App...

---
+++ SpringBoot-FirstApp/src/main/java/base/model/Employee.java
@@ -0,0 +1,10 @@
+package base.model;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class Employee {
+	public Employee() {
+		System.out.println("EMPLOYEE CONSTRUCTOR");
+	}
+}

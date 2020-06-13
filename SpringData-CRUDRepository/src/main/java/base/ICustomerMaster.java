199289c67be6a01f245139149c5e59d3ae3dd694
SAM <sam062>
2020-06-13 15:03:50 +0530

    Modified and added more method executions ...

--- SpringData-CRUDRepository/src/main/java/base/ICustomerMaster.java
+++ SpringData-CRUDRepository/src/main/java/base/ICustomerMaster.java
@@ -1,6 +1,7 @@
 package base;
 
 import java.io.Serializable;
+import java.util.List;
 
 import org.springframework.data.repository.CrudRepository;
 
@@ -8,5 +9,9 @@ import base.entity.CustomerMasterEntity;
 
 public interface ICustomerMaster extends CrudRepository<CustomerMasterEntity, Serializable> {
 	public CustomerMasterEntity findByCustName(String s);
+	public CustomerMasterEntity findByCustNameAndCustMobile(String name,String mobile);
+	public CustomerMasterEntity findByCustNameOrCustMobile(String name,String mobile);
+	public List<CustomerMasterEntity> findByCustNameStartsWith(String prefix);
+
 }
 
--- SpringData-CRUDRepository/src/main/java/base/SpringDataCrudRepositoryApplication.java
+++ SpringData-CRUDRepository/src/main/java/base/SpringDataCrudRepositoryApplication.java
@@ -1,6 +1,7 @@
 package base;
 
 import java.util.Arrays;
+import java.util.List;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -11,7 +12,7 @@ import base.entity.CustomerMasterEntity;
 @SpringBootApplication
 public class SpringDataCrudRepositoryApplication {
 
-	//---------------------------------FIND BY ID 
+	//***************************************FIND BY ID 
 	//	public static void main(String[] args) {
 	//		ConfigurableApplicationContext con=SpringApplication.run(SpringDataCrudRepositoryApplication.class, args);
 	//
@@ -30,7 +31,7 @@ public class SpringDataCrudRepositoryApplication {
 	//			System.out.println("NOT FOUND");
 	//	}
 
-	//-------------------------------FIND ALL BY ID's
+	//**********************************FIND ALL BY ID's
 	//	public static void main(String[] args) {
 	//		ConfigurableApplicationContext con=SpringApplication.run(SpringDataCrudRepositoryApplication.class, args);
 	//
@@ -43,17 +44,10 @@ public class SpringDataCrudRepositoryApplication {
 	//	}
 
 
-	//-------------------------------FIND BY COLUMN NAME
-	//	public static void main(String[] args) {
-	//		ConfigurableApplicationContext con=SpringApplication.run(SpringDataCrudRepositoryApplication.class, args);
-	//
-	//		ICustomerMaster icm=con.getBean(ICustomerMaster.class);
-	//		CustomerMasterEntity entity = icm.findByCustName("SAM");
-	//		System.out.println("---------------------"+entity);
-	//	}
-	//	
-	//	
-	//	//-------------------------------FIND ALL
+
+
+
+	//	**********************************FIND ALL
 	//		public static void main(String[] args) {
 	//			ConfigurableApplicationContext con=SpringApplication.run(SpringDataCrudRepositoryApplication.class, args);
 	//	
@@ -63,9 +57,9 @@ public class SpringDataCrudRepositoryApplication {
 	//				 System.out.println(entity);
 	//			 });
 	//		}
-		
-		
-	//	//-------------------------------SAVE AND UPDATE DATA
+
+
+	//	**********************************SAVE AND UPDATE DATA
 	//	public static void main(String[] args) {
 	//		ConfigurableApplicationContext con=SpringApplication.run(SpringDataCrudRepositoryApplication.class, args);
 	//
@@ -77,25 +71,57 @@ public class SpringDataCrudRepositoryApplication {
 	//		entity.setCustMobile("8181861314");
 	//		icm.save(entity);
 	//	}
-		
-		
-	//-------------------------------SAVE ALL DATA
+
+
+	//**********************************SAVE ALL DATA
+	//	public static void main(String[] args) {
+	//		ConfigurableApplicationContext con=SpringApplication.run(SpringDataCrudRepositoryApplication.class, args);
+	//
+	//		ICustomerMaster icm=con.getBean(ICustomerMaster.class);
+	//
+	//		CustomerMasterEntity entity1=new CustomerMasterEntity();
+	//		entity1.setCustId(106);
+	//		entity1.setCustName("MUM");
+	//		entity1.setCustMobile("8004737178");
+	//
+	//		CustomerMasterEntity entity2=new CustomerMasterEntity();
+	//		entity2.setCustId(107);
+	//		entity2.setCustName("PAA");
+	//		entity2.setCustMobile("9935416983");
+	//
+	//		icm.saveAll(Arrays.asList(entity1,entity2));
+	//
+	//	}
+
+
+
+	//**********************************FIND BY COLUMN NAME
+	//	public static void main(String[] args) {
+	//		ConfigurableApplicationContext con=SpringApplication.run(SpringDataCrudRepositoryApplication.class, args);
+	//
+	//		ICustomerMaster icm=con.getBean(ICustomerMaster.class);
+	//		CustomerMasterEntity entity = icm.findByCustName("SAM");
+	//		System.out.println("---------------------"+entity);
+	//	}
+
+
+
+	//**********************************FIND BY MULTIPLE COLUMN NAMES
+	//	public static void main(String[] args) {
+	//		ConfigurableApplicationContext con=SpringApplication.run(SpringDataCrudRepositoryApplication.class, args);
+	//		ICustomerMaster icm=con.getBean(ICustomerMaster.class);
+	//		
+	//		CustomerMasterEntity entity = icm.findByCustNameOrCustMobile("SAM", "123");
+	//		System.out.println("---------------------"+entity);
+	//	}
+
+
+
+	//**********************************FIND BY NAME PREFIX(STARTS WITH ..)
 	public static void main(String[] args) {
 		ConfigurableApplicationContext con=SpringApplication.run(SpringDataCrudRepositoryApplication.class, args);
-		
 		ICustomerMaster icm=con.getBean(ICustomerMaster.class);
-		
-		CustomerMasterEntity entity1=new CustomerMasterEntity();
-		entity1.setCustId(106);
-		entity1.setCustName("MUM");
-		entity1.setCustMobile("8004737178");
-		
-		CustomerMasterEntity entity2=new CustomerMasterEntity();
-		entity2.setCustId(107);
-		entity2.setCustName("PAA");
-		entity2.setCustMobile("9935416983");
-		
-		icm.saveAll(Arrays.asList(entity1,entity2));
-		
+		List<CustomerMasterEntity> list=icm.findByCustNameStartsWith("SS");
+		list.forEach(entity->System.out.println(entity));
 	}
 }

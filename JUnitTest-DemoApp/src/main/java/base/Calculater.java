8a9db4d6aa48d2c05050617eb876a8aa5510ad8d
SAM <sam062>
2020-06-14 22:22:32 +0530

    Modified With Init and Destroy methods..

--- JUnitTest-DemoApp/src/main/java/base/Calculater.java
+++ JUnitTest-DemoApp/src/main/java/base/Calculater.java
@@ -5,5 +5,10 @@ public class Calculater {
 	public Integer add(int a, int b) {
 		return a+b;
 	}
+	
+	public Integer mul(int a, int b) {
+		return a*b;
+	}
+	
 
 }
--- JUnitTest-DemoApp/src/test/java/base/test/TestCalculater.java
+++ JUnitTest-DemoApp/src/test/java/base/test/TestCalculater.java
@@ -2,17 +2,33 @@ package base.test;
 
 import static org.junit.Assert.assertEquals;
 
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
 import org.junit.Test;
 
 import base.Calculater;
 
 public class TestCalculater {
+	public static Calculater c=null;
+	@BeforeClass
+	public static void  init() {
+		c=new Calculater();
+	}
+	
 	@Test
 	public void testCal() {
-		
-		Calculater c=new Calculater();
 		int sum=c.add(10, 20);
 		assertEquals(30, sum);
+	}
+	
+	@Test
+	public void testMul() {
+		int mul=c.mul(10, 10);
+		assertEquals(100, mul);
+	}
+	@AfterClass
+	public static void destroy() {
+		c=null;
 	}
 
 }

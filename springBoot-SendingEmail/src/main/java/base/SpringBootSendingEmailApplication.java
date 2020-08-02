c4bfb6b805c1bf500becab3a6e24aa1ad146d382
SAM <sam062>
2020-08-02 15:24:41 +0530

    Added Mime msg sending.

--- springBoot-SendingEmail/src/main/java/base/SpringBootSendingEmailApplication.java
+++ springBoot-SendingEmail/src/main/java/base/SpringBootSendingEmailApplication.java
@@ -12,8 +12,16 @@ public class SpringBootSendingEmailApplication {
 	public static void main(String[] args) {
 		ConfigurableApplicationContext run = SpringApplication.run(SpringBootSendingEmailApplication.class, args);
 		EmailUtils bean = run.getBean(EmailUtils.class);
-		Boolean emailSent = bean.isEmailSent("ip6.sam@gmail.com", "TESTING MAIL", "SENT FROM SPRING BOOT APP");
+		
+//		Boolean emailSent = bean.isEmailSent("ip6.sam@gmail.com", "TESTING MAIL", "SENT FROM SPRING BOOT APP");
+		
+		Boolean emailSent = bean.isMimeEmailSent("ip6.sam@gmail.com", "<h1>TESTING MAIL</h1>", "<b>SENT FROM SPRING BOOT APP</b>");
+		
+		
+		
 		System.out.println("**** MAIL SENT**** "+emailSent);
+		
+		
 	}
 
 }
--- springBoot-SendingEmail/src/main/java/base/util/EmailUtils.java
+++ springBoot-SendingEmail/src/main/java/base/util/EmailUtils.java
@@ -1,8 +1,11 @@
 package base.util;
 
+import javax.mail.internet.MimeMessage;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.stereotype.Component;
 
 @Component
@@ -18,6 +21,27 @@ public class EmailUtils {
 			msg.setText(body);
 
 			javaMailSender.send(msg);
+			return true;
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return false;
+	}
+
+	public Boolean isMimeEmailSent(String to, String subject, String body) {
+		try {
+			MimeMessage msg=javaMailSender.createMimeMessage();
+			MimeMessageHelper msgHelper = new MimeMessageHelper(msg);
+			String[] recip=new String[2];
+			recip[0]="ip6.sam@gmail.com";
+			recip[1]="sam9695654346@gmail.com";
+
+			msgHelper.setTo(recip);
+			msgHelper.setSubject(subject);
+			msgHelper.setText(body,true);
+
+			javaMailSender.send(msg);
+
 			return true;
 		} catch (Exception e) {
 			e.printStackTrace();
